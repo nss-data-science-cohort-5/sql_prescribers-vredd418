b83a6b212c6a3ab9be6870d9720f007a0108647a
vredd418 <vredd418@gmail.com>
2022-02-05 14:05:00 -0600

    Difficult question in prescribers done. Finished part 2 through question 2. Used jupyter notebook for part 2.

--- prescribers.sql
+++ prescribers.sql
@@ -1,7 +1,7 @@
 /* 1. a. Which prescriber had the highest total number of claims (totaled over all drugs)? Report the npi and the total number of claims.
       b. Repeat the above, but this time report the nppes_provider_first_name, nppes_provider_last_org_name,  specialty_description, and the total number of claims. */
 
-SELECT SUM(total_claim_count), npi, nppes_provider_first_name, nppes_provider_last_org_name,  specialty_description
+SELECT SUM(total_claim_count), npi, nppes_provider_first_name || ' ' || nppes_provider_last_org_name AS name, specialty_description -- Concat two columns into one with space separator
 FROM prescription
 LEFT JOIN prescriber
 USING(npi)
@@ -27,7 +27,7 @@ GROUP BY specialty_description
 ORDER BY SUM(total_claim_count) DESC
 LIMIT 1;
 
-SELECT specialty_description, SUM(total_claim_count)
+SELECT specialty_description, TO_CHAR(SUM(total_claim_count), 'FM9,999,999') -- Adds commas to numbers, but converts to text
 FROM prescription
 LEFT JOIN prescriber
 USING(npi)
@@ -38,18 +38,105 @@ GROUP BY specialty_description
 ORDER BY SUM(total_claim_count) DESC
 LIMIT 1;
 
-SELECT npi, speciality_description, opioid_drug_flag
+SELECT speciality_description opioid_drug_flag
+(SELECT  specialty_description, opioid_drug_flag, SUM(total_claim_count) AS total_claims
+FROM prescriber
+LEFT JOIN prescription
+USING(npi)
+LEFT JOIN drug
+USING(drug_name)
+GROUP BY specialty_description, opioid_drug_flag) AS sub
+
+-- James, 2c
+SELECT DISTINCT specialty_description, SUM(total_claim_count) AS total_claims
+FROM prescriber
+FULL JOIN prescription
+ON prescriber.npi = prescription.npi
+GROUP BY specialty_description
+HAVING (SUM(total_claim_count) IS NULL)
+ORDER BY total_claims;
+
+-- Jacob, 2c
+SELECT DISTINCT specialty_description
+FROM prescriber
+WHERE specialty_description NOT IN 
+	(SELECT specialty_description
+	FROM prescription
+	INNER JOIN prescriber
+	USING (npi));
+
+-- Chris M., 2c
+SELECT p1.specialty_description AS specialty, COALESCE(SUM(p2.total_claim_count), 0) AS total_claims
+FROM prescriber p1
+LEFT JOIN prescription p2 ON p1.npi = p2.npi
+GROUP BY 1
+ORDER BY 2;
+
+-- Ross, 2c
+SELECT DISTINCT pr.specialty_description, COUNT(pn.total_claim_count) AS claim_cnt
+FROM prescriber pr
+LEFT JOIN prescription pn
+ON pr.npi = pn.npi
+-- WHERE SUM(pn.total_claim_count) IS NULL
+GROUP BY pr.specialty_description
+HAVING COUNT(pn.total_claim_count) = 0
+ORDER BY claim_cnt DESC;
+
+WITH 
+	cte_1 AS (
+		SELECT 
+			specialty_description, 
+			SUM(total_claim_count) AS total_opioid_claims
+		FROM prescriber
+		JOIN prescription
+		USING(npi)
+		JOIN drug
+		USING(drug_name)
+		WHERE opioid_drug_flag = 'Y'
+		GROUP BY specialty_description, opioid_drug_flag
+),
+	cte_2 AS (
+		SELECT 
+			specialty_description,
+			SUM(total_claim_count) AS total_claims
+		FROM prescriber
+		JOIN prescription
+		USING(npi)
+		GROUP BY specialty_description	
+)
+SELECT 
+	specialty_description,
+	ROUND(total_opioid_claims / total_claims * 100.0, 2) AS opioid_pct
+FROM cte_1
+JOIN cte_2
+USING(specialty_description)
+ORDER BY opioid_pct DESC
+
+-- Jacob
+SELECT 
+	specialty_description, 
+	ROUND(SUM(opioid_claims)/SUM(total_claim_count) * 100.0 , 2) AS percent_opioid
+FROM
+(SELECT specialty_description, total_claim_count, drug_name, opioid_drug_flag,
+	CASE WHEN opioid_drug_flag = 'N' THEN 0
+	ELSE total_claim_count END AS opioid_claims
+FROM prescriber
+INNER JOIN prescription
+USING(npi)
+INNER JOIN drug
+USING (drug_name)) AS opioid_claims_table
+GROUP BY specialty_description
+ORDER BY percent_opioid DESC;
 
 
 /* 3. a. Which drug (generic_name) had the highest total drug cost? -- INSULIN GLARGINE,HUM.REC.ANLOG, $104264066.35 
 
     b. Which drug (generic_name) has the hightest total cost per day? **Bonus: Round your cost per day column to 2 decimal places. Google ROUND to see how this works. -- IMMUN GLOB G(IGG)/GLY/IGA OV50, $ 7141.11 per day */
 	
-SELECT generic_name, SUM(total_drug_cost)
+SELECT generic_name, total_drug_cost
 FROM prescription
 LEFT JOIN drug
 USING(drug_name)
-GROUP BY generic_name
 ORDER BY 2 DESC
 LIMIT 1;
 
@@ -69,25 +156,26 @@ ORDER BY 2 DESC
 LIMIT 1;
 
 -- Alex's answer again.
-SELECT generic_name, ROUND(SUM(total_drug_cost)/SUM(total_day_supply), 2)
+SELECT generic_name, ROUND(SUM(total_drug_cost)/SUM(total_day_supply), 2)::MONEY -- Cast as money
 FROM prescription
 LEFT JOIN drug
 USING (drug_name)
 GROUP BY generic_name
-ORDER BY 2 DESC;
+ORDER BY 2 DESC
+LIMIT 1;
 
 
 /* 4. a. For each drug in the drug table, return the drug name and then a column named 'drug_type' which says 'opioid' for drugs which have opioid_drug_flag = 'Y', says 'antibiotic' for those drugs which have antibiotic_drug_flag = 'Y', and says 'neither' for all other drugs.
 
     b. Building off of the query you wrote for part a, determine whether more was spent (total_drug_cost) on opioids or on antibiotics. Hint: Format the total costs as MONEY for easier comparision. -- More spent on opioids. */
 
-SELECT sub.drug_type, CAST(SUM(total_drug_cost) AS money)
+SELECT sub.drug_type, CAST(SUM(total_drug_cost) AS money) -- Some drugs listed as opioid and not opioid, so answer slightly different depending on query
 FROM prescription
-LEFT JOIN
+JOIN
 	(SELECT drug_name,
-	   	CASE WHEN opioid_drug_flag = 'Y' THEN 'opioid'
-	         WHEN antibiotic_drug_flag = 'Y' THEN 'antibiotic'
-	         ELSE 'neither' END AS drug_type
+	 CASE WHEN opioid_drug_flag = 'Y' THEN 'opioid'
+	 WHEN antibiotic_drug_flag = 'Y' THEN 'antibiotic'
+	 ELSE 'neither' END AS drug_type
 	 FROM drug) AS sub
 USING(drug_name)
 GROUP BY sub.drug_type
@@ -111,9 +199,26 @@ USING(fipscounty)
 GROUP BY cbsaname
 ORDER BY sum_pop;
 
-SELECT sub.cbsa, sub.county, sub.population
+-- Bryan for 5b, returns only smallest and largest
+select * 
+from (select cbsa.cbsaname, sum(population) total_population
+      from cbsa
+               inner join population p on cbsa.fipscounty = p.fipscounty
+      group by cbsa.cbsaname
+      order by total_population desc
+      limit 1) sq1
+union
+select *
+from (select cbsa.cbsaname, sum(population) total_population
+      from cbsa
+               inner join population p on cbsa.fipscounty = p.fipscounty
+      group by cbsa.cbsaname
+      order by total_population asc
+      limit 1) sql;
+
+SELECT sub.county, sub.population
 FROM 
-	(SELECT fipscounty, population, county, cbsa
+	(SELECT population, county, cbsa
 	 FROM fips_county
 	 JOIN population
 	 USING(fipscounty)
@@ -181,4 +286,6 @@ WHERE specialty_description = 'Pain Management'
 ) as npi_drug
 LEFT JOIN prescription
 USING(npi, drug_name);
+
+
 
